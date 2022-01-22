07185ce840980dc884cbeb93d6daf9c99ee30736
Pedro Ballona <pedro.ballona@totvs.com.br>
2022-01-22 18:57:40 -0300

    Fix bug

--- script.js
+++ script.js
@@ -4,27 +4,31 @@ window.addEventListener("load", function () {
 });
 
 function initializeGallery() {
-  console.log("here");
   const instance = lightGallery(document.getElementById("lightgallery"), {
     plugins: [lgZoom, lgThumbnail],
     licenseKey: "your_license_key",
     speed: 500,
-    zoomFromOrigin: true,
   });
   const galleryInstance = [...instance.galleryItems];
   galleryInstance.splice(galleryInstance.length - 1, 1);
-  instance.updateSlides(
-    galleryInstance,
-    instance.index,
-  );
+  instance.updateSlides(galleryInstance, instance.index);
 }
 
 function initMasonry() {
   const grid = document.querySelector("#lightgallery");
   const msnry = new Masonry(grid, {
     itemSelector: "#lightgallery > a",
-    columnWidth: '.grid-sizer',
+    columnWidth: ".grid-sizer",
     percentPosition: true,
-    gutter: 10
+    gutter: 10,
   });
+  rxjs
+    .fromEvent(window, "resize")
+    .pipe(rxjs.debounceTime(500))
+    .subscribe(() => {
+      msnry.layout();
+    });
+  this.setTimeout(() => {
+    msnry.layout();
+  }, 2000);
 }
