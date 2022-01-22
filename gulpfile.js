01f9f56e622f7e770515f116c408e05bd58c60bd
Pedro Ballona <pedro.ballona@totvs.com.br>
2022-01-22 19:02:56 -0300

    Fix bug

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ function copyRoot() {
 function copyImages() {
   return gulp
     .src(["images/**/*"])
-    .pipe(imagemin())
+    // .pipe(imagemin())
     .pipe(gulp.dest("dist/images"));
 }
 
@@ -27,11 +27,17 @@ function copyMasonry() {
     .pipe(gulp.dest("dist/node_modules/masonry-layout/dist"));
 }
 
+function copyRxjs() {
+  return gulp
+    .src(["./node_modules/rxjs/**/*"])
+    .pipe(gulp.dest("dist/node_modules/rxjs"));
+}
+
 function clear() {
   return gulp.src("dist", { read: false, allowEmpty: true }).pipe(gulpClean());
 }
 
 export const build = gulp.series([
   clear,
-  gulp.parallel([copyRoot, copyLightGallery, copyMasonry, copyImages]),
+  gulp.parallel([copyRoot, copyLightGallery, copyMasonry, copyImages, copyRxjs]),
 ]);
